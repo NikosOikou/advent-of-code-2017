7f075e80d7e13bd7f71a97a0f5439c68a8a63ae1
nikos <nicolaosoikou@gmail.com>
2017-12-04 13:14:10 +0100

    Removed print statements

--- 1.2/solution.py
+++ 1.2/solution.py
@@ -6,7 +6,6 @@ total_sum = 0
 for i, digit in enumerate(n[:l]):
     digit = int(digit)
     next_digit = int(n[i+h])
-    print digit, next_digit
     if digit == next_digit:
         total_sum += digit
 print total_sum
