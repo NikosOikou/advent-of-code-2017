f532790b0d7247cbe61e10c653e0893e7f626755
nikos <nicolaosoikou@gmail.com>
2017-12-06 07:53:49 +0100

    Add solution for 6.1

---
+++ 6.1/solution.py
@@ -0,0 +1,20 @@
+banks = map(int, open('input.txt').read().strip().split())
+states_seen = set()
+n_banks = len(banks)
+count = 0
+while True:
+    count += 1
+    n_to_allocate = max(banks)
+    idx = banks.index(n_to_allocate)
+    for i in range(n_to_allocate):
+        if i == 0:
+            banks[idx] = 0
+        idx += 1
+        if idx > n_banks - 1:
+            idx = 0
+        banks[idx] += 1
+    s = str(banks) 
+    if s in states_seen:
+        break
+    states_seen.add(s)
+print count
